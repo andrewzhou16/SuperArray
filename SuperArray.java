c088be6526c3e63daa5dcc1490db8b6ca82cdd74
andrewzhou16 <48416885+andrewzhou16@users.noreply.github.com>
2019-05-10 20:16:37 -0400

    Add files via upload

--- SuperArray.java
+++ SuperArray.java
@@ -1,1 +1,113 @@
+import java.util.array;
+class SuperArray{
+  int[] intarray = new int[10];
+  String[] stringarray = new String[10];
 
+  public void clear(){
+    var list = [1,2,3,4,5,6,7,8,9,10];
+    list.length = null;
+  }
+  public int intSize(){
+    int[] intarray = i;
+
+  }
+  public boolean isEmpty(){
+    if (var list = [0]);
+    return true;
+    if (var list > [0]);
+    return false;
+  }
+  public boolean add(String element){
+    var list = [i];
+    String[i] = new String[i+1];
+    list.add("new value");
+  }
+  public String stringGet(int index){
+    int index = [x];
+      System.out.println(String [x]);
+        try{String[] array = new String[1];
+          array[3]= 2;
+        }
+        catch (ArithmeticException e){
+          System.out.println("Index out of range");
+  }
+  public String stringSet(int index, String element){
+
+  }
+  public String toString(){
+    System.out.print("[")
+    for(i=0; i<Stringarray.length;i++){
+    System.out.print(Stringarray[i]+",");
+    System.out.print("]")
+  }
+  }
+  public boolean booleanContains(String element){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Element Search");
+    element = register.nextLine();
+    for(i=0; i<Stringarray.length; i++){
+      if i = element{
+        return true;
+      }
+    }
+  }
+  public void add(int index, String element){
+    Stringarray[i] = new Stringarray[i+1];
+    for(i=0; i<Stringarray.length; i++){
+      if(i === null){
+      }
+    }
+  }
+  public int indexOfString(String element){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Element Search");
+    element = register.nextLine();
+    for(i=0; i<Stringarray.length; i++){
+      if i = element{
+        return i;
+      }
+    else return -1;
+  }
+  }
+  public int lastIndexOfString(String element){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Element Search");
+    element = register.nextLine();
+    for(i=0; i<Stringarray.length; i++){
+      if Math.abs i> Math.abs i = element{
+        return i;
+      }
+    else return -1;
+  }
+  public String remove(int index){
+    if(index >= size){
+            throw new ArrayIndexOutOfBoundsException("index array out of bounds");
+    }
+    String removed = data[index];
+    String[] data2 = new String[size - 1];
+    for(int i = 0; i < index; i ++){
+                data2[i] = data[i];
+    }
+    for(int i = index + 1; i < size; i ++){
+                data2[i-1] = data[i]
+    }
+    return removed;
+  }
+
+  public boolean remove(String element){
+    String[] data2 = new String[size -1];
+    for(int i = 0; i< size; i ++);
+        if(element == data[i]){
+                  int index = 1;
+                  for(int j = 0; j < index; j++){
+                      data2[j] = data[j];
+                  }
+                  for(int j = index + !; j < size; j++){
+                      data2[j=1] = data[j];
+                  }
+                  return true;
+        }
+  }
+  return false;
+}
+}
